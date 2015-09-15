3cb3064f9fc169da019eb4c57c98f430c11eba02
Ratnadeep Deshmane <ratnadeepdeshmane@gmail.com>
2015-09-15 15:39:52 -0400

    docebo sso_links imporoved to take options as well

--- lib/docebo_ruby/user.rb
+++ lib/docebo_ruby/user.rb
@@ -21,10 +21,10 @@ module DoceboRuby
         end
       end
 
-      def sso_url(username)
+      def sso_url(username, options = {})
         timestamp = Time.now.to_i
         token = Digest::MD5.hexdigest([username, timestamp, DoceboRuby.config.sso_token].join(','))
-        "http://#{DoceboRuby.config.saas_url}/doceboLms/index.php?auth_regen=1&modname=login&op=confirm&login_user=#{username}&time=#{timestamp}&token=#{token}"
+        "http://#{DoceboRuby.config.saas_url}/doceboLms/index.php?auth_regen=1&modname=login&op=confirm&login_user=#{username}&time=#{timestamp}&token=#{token}&course=#{options[:course]}&destination=#{options[:destination]}"
       end
     end
   end
