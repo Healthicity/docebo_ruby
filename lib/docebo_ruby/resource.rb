f6f3f40edfaba23c3cf90943747ff0effd8a9e99
Ratnadeep Deshmane <ratnadeepdeshmane@gmail.com>
2015-09-16 15:11:50 -0400

    retry loop added to handle the api timeout

--- lib/docebo_ruby/resource.rb
+++ lib/docebo_ruby/resource.rb
@@ -9,11 +9,15 @@ module DoceboRuby
     end
 
     def self.fetch_data(method, params = {}, &block)
+      tries ||= 3
       @fetcher = API.new
       @fetcher.send_request(@api, method, params) do |result|
         return result unless block_given?
         yield result
       end
+    rescue RestClient::RequestTimeout => e
+      sleep (4 - tries)
+      retry unless (tries -= 1).zero?
     end
   end
 end
